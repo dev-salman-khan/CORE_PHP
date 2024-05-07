a09352b56a838caaaa2b576569f217c765c4b8a2
Md Salman Khan <salmancode79@gmail.com>
2024-05-07 12:40:27 -0400

    Data Type Code

---
+++ data_type.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <?php
+            # variable dump funtion
+            $string_fetch = 'salman';
+            var_dump($string_fetch);
+            print("<br>");
+            print("<br>");
+            print("<br>");
+            #array 
+            $company = array("intel","DEll","Intex");
+            var_dump($company);
+            print("<br>");
+            print("<br>");
+            print("<br>");
+
+                // two same variable 
+            $x = 5;
+            var_dump($x);
+            echo "<br>";
+            $x = "Hello";
+            var_dump($x);
+        ?>
+
+</body>
+</html>
